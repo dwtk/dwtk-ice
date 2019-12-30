89b6a29d5904812beb822aa9026dc6055738f1a2
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2019-12-30 23:25:19 +0100

    Use our new USB VID/PID. Thanks to OpenMoko

--- usbconfig.h
+++ usbconfig.h
@@ -226,7 +226,7 @@ section at the end of this file).
 
 /* -------------------------- Device Description --------------------------- */
 
-#define  USB_CFG_VENDOR_ID       0xc0, 0x16 /* = 0x16c0 = 5824 = voti.nl */
+#define  USB_CFG_VENDOR_ID       0x50, 0x1d /* = 0x1d50 = 7504 = OpenMoko, Inc. */
 /* USB vendor ID for the device, low byte first. If you have registered your
  * own Vendor ID, define it here. Otherwise you may use one of obdev's free
  * shared VID/PID pairs. Be sure to read USB-IDs-for-free.txt for rules!
@@ -235,7 +235,7 @@ section at the end of this file).
  * with libusb: 0x16c0/0x5dc.  Use this VID/PID pair ONLY if you understand
  * the implications!
  */
-#define  USB_CFG_DEVICE_ID       0xdc, 0x05 /* = 0x05dc = 1500 */
+#define  USB_CFG_DEVICE_ID       0x4c, 0x61 /* = 0x614c = 24908 = dwtk In-Circuit Emulator */
 /* This is the ID of the product, low byte first. It is interpreted in the
  * scope of the vendor ID. If you have registered your own VID with usb.org
  * or if you have licensed a PID from somebody else, define it here. Otherwise
